cf4a9428670c19ab88d958ddb0a7a3ce27da8198
somi <somi@somi.local>
2017-12-13 18:36:13 +0900

    오타수정
    오타수정

--- kinder_and_care/episode/ep_03.js
+++ kinder_and_care/episode/ep_03.js
@@ -9,7 +9,7 @@ function build_ep_03() {
   
   let html = `
     <p>노원구에는 <q0>${num_care}개의 어린이집</q0>과 <q1>${num_kinder}개의 유치원</q1>이 있고, 그 중 <q2>${num_closed}개의 유치원</q2>이 최근 몇 년 사이에 폐원을 해서 총 <q3>${num_school}개의 어린이를 맡길 수 있는 기관</q3>이 운영 중 입니다.</p>
-    <p>왼쪽 지도에 나타나는 표시에 마우스를 올려 어린이과 유치원의 정보를 좀 더 자세히 탐색할 수 있습니다.</p>
+    <p>왼쪽 지도에 나타나는 표시에 마우스를 올려 어린이집과 유치원의 정보를 좀 더 자세히 탐색할 수 있습니다.</p>
     <p><next>다음 →</next></p>
   `;
   el.innerHTML = html;
@@ -28,7 +28,7 @@ function build_ep_03() {
   
   el.querySelectorAll('q0').forEach(q => {
     q.className = 'q';
-    q.style['border-bottom'] = '2px solid rgb(0,255,255)';
+    q.style['border-bottom'] = '2px solid rgb(255,255,0)';
     q.onmouseover = function(e) {
       wait_episode = wait_episode_dur*0.75|0;
       if(anim_gens.length > 0) return;
@@ -57,7 +57,7 @@ function build_ep_03() {
   
   el.querySelectorAll('q1').forEach(q => {
     q.className = 'q';
-    q.style['border-bottom'] = '2px solid rgb(255,255,0)';
+    q.style['border-bottom'] = '2px solid rgb(0,255,255)';
     q.onmouseover = function(e) {
       wait_episode = wait_episode_dur*0.75|0;
       if(anim_gens.length > 0) return;
